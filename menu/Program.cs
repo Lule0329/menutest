67dc6a83a2fecb7404ba3b7fd3ff3b4e5c1d03c7
Ludwig Lennartsson <ludwig.lennartsson@skola.sala.se>
2024-08-22 19:01:47 +0200

    Add project files.

---
+++ menu/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteMenu1();
+            WriteMenu();
+        }
+
+        static void WriteMenu()
+        {
+            int index = 0;
+
+            if (Console.ReadKey().Key == ConsoleKey.UpArrow)
+            {
+                if (index == 0)
+                {
+                    index++;
+                    WriteMenu3();
+                }
+                else if (index == 1)
+                {
+                    index++;
+                    WriteMenu1();
+                }
+                else if (index == 2)
+                {
+                    WriteMenu2();
+                }
+            }
+        }
+
+        static void WriteMenu1()
+        {
+            Console.Clear();
+
+            Console.WriteLine(">test1");
+            Console.WriteLine(" test2");
+            Console.WriteLine(" test3");
+
+            WriteMenu();
+        }
+
+        static void WriteMenu2()
+        {
+            Console.Clear();
+            
+            Console.WriteLine(" test1");
+            Console.WriteLine(">test2");
+            Console.WriteLine(" test3");
+            
+            WriteMenu();
+        }
+
+        static void WriteMenu3()
+        {
+            Console.Clear();
+
+            Console.WriteLine(" test1");
+            Console.WriteLine(" test2");
+            Console.WriteLine(">test3");
+
+            WriteMenu();
+        }
+    }
+}
